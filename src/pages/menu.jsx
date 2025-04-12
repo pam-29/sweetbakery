d0179422c83c5f5ce684457b06d72475811c28eb
pam-29 <pamelaarijaona@gmail.com>
2025-04-12 12:46:25 +0200

    change file

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { MenuList } from "../helpers/menuList";
-import MenuItem from "../components/MenuItem";
+import { MenuList } from "../helpers/MenuList";
+import MenuItem from "../components/menuItem";
 import "../styles/menu.css";
 
 function Menu() {
     return (
         <div className="menu">
-            <h1 className="menuTitle">Nos donuts</h1>
-            <div className="menuList">
-                {MenuList.map((menuItem, key) => {
-                return (
-                    <MenuItem
-                    key={key}
-                    image={menuItem.image}
-                    name={menuItem.name}
-                    price={menuItem.price}
-                    />
-                );
-                })}
-            </div>
+        <h1 className="menuTitle">Nos donuts</h1>
+        <div className="menuList">
+            {MenuList.map((menuItem, key) => {
+            return (
+                <MenuItem
+                key={key}
+                image={menuItem.image}
+                name={menuItem.name}
+                price={menuItem.price}
+                />
+            );
+            })}
+        </div>
         </div>
     );
 }
