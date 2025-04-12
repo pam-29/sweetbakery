06b852ee0d85a8b87691c9e003e02bbc2a7cd7bd
pam-29 <pamelaarijaona@gmail.com>
2025-04-12 11:04:47 +0200

    fix responsive

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,7 +5,10 @@ import '../styles/about.css'
 function about() {
     return (
         <div className='about'>
-            <div className='aboutTop' style={{ backgroundImage: `url(${BannerImg})` }}></div>
+            <div className='aboutTop'>
+                <img src={BannerImg} alt="image donut" />
+            </div>
+
             <div className='aboutBottom'>
                 <h1>A propos</h1>
                 <p>Bienvenue chez Sweet Bakery, le paradis des donuts gourmands ! Passionnés de pâtisserie et amoureux des saveurs uniques, nous vous proposons une sélection artisanale de donuts moelleux et savoureux, préparés avec des ingrédients de qualité. Que vous soyez fan de chocolat fondant, de fruits acidulés ou de glaçages créatifs, notre collection saura ravir vos papilles. <br /> <br />
