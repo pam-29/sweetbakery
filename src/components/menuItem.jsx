a979127aef545b7e68d657d523e518153722f7d8
pam-29 <pamelaarijaona@gmail.com>
2025-04-12 11:55:00 +0200

    Corrigé le composant MenuItem

--- src/components/menuItem.jsx
+++ src/components/menuItem.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function MenuItem({ image, name, price }) {
     return (
         <div className="menuItem">
-        <div style={{ backgroundImage: `url(${image})` }}> </div>
-        <h1> {name} </h1>
-        <p> ${price} </p>
+            <div style={{ backgroundImage: `url(${image})` }}></div>
+            <h1>{name}</h1>
+            <p>${price}</p>
         </div>
     );
 }
